df5f5cfd209081d6b3284c43817800a060fc8fa1
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-09-05 14:14:51 -0400

    test cupertinoPane

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.hello').addEventListener('click', function(){
+    alert('hello');
+});
+
+document.querySelector('cupertino').addEventListener('click',  function() {
+    alert('hello');
+    console.log('hello');
+})
